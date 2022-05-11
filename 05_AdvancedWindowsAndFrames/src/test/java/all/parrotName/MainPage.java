4b06946f4d08b7b4ba92310ad6617129c63d6ea9
Alex <alexandr-makedonsky@yandex.ru>
2022-05-11 17:43:41 +0300

    add05HM

---
+++ 05_AdvancedWindowsAndFrames/src/test/java/all/parrotName/MainPage.java
@@ -0,0 +1,75 @@
+package all.parrotName;
+import all.base.pages.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MainPage extends Page {
+
+    private String url = "http://qajava.skillbox.ru/module5/homework/";
+
+    @FindBy(tagName = "iframe")
+    public WebElement iframeElement;
+    @FindBy(id = "boy")
+    public WebElement boy;
+    @FindBy(id = "girl")
+    public WebElement girl;
+    @FindBy(name = "email")
+    public WebElement inputEmail;
+    @FindBy(id = "sendMe")
+    public WebElement submitButton;
+    @FindBy(css = ".your-email")
+    public WebElement resultTitle;
+    @FindBy(css = ".form-error")
+    public WebElement errorMessage;
+    @FindBy(css = ".socialLinks__link:nth-of-type(1)")
+    public WebElement facebookIcon;
+    @FindBy(css = ".socialLinks__link:nth-of-type(2)")
+    public WebElement vkIcon;
+    @FindBy(css = ".socialLinks__link:nth-of-type(3)")
+    public WebElement tweeterIcon;
+    @FindBy(css = ".socialLinks__link:nth-of-type(4)")
+    public WebElement instaIcon;
+    @FindBy(css = ".footer__contactItem:nth-of-type(2)")
+    public WebElement skillboxLink;
+    @FindBy(css = ".header")
+    public WebElement header;
+
+    public MainPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {driver.navigate().to(url);}
+
+    public void switchToFrame() {
+        driver.switchTo().frame(iframeElement);
+    }
+
+    public void addEmail() {
+      inputEmail.sendKeys("test@skillbox.ru");
+    }
+
+    public void addInvalidEmail(String value) {
+        inputEmail.sendKeys(value);
+    }
+
+    public String getTitleText() {
+        return resultTitle.getText();
+    }
+
+    public String getErrorMessageText() {
+        return errorMessage.getText();
+    }
+
+    public void switchToMainPage() {
+        driver.switchTo().defaultContent();
+    }
+
+    public void switchToParrentFrame() {
+        driver.switchTo().parentFrame();
+    }
+}
---
+++ 05_AdvancedWindowsAndFrames/src/test/java/all/parrotName/Tests.java
@@ -0,0 +1,212 @@
+package all.parrotName;
+import all.base.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class Tests extends TestBase {
+
+    @Test
+    public void getBoyName__test() {
+        //arrange
+        var expectedTitle = "test@skillbox.ru";
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.boy.click();
+        page.addEmail();
+        page.submitButton.click();
+        //assert
+        var actualResult = page.getTitleText();
+        Assertions.assertEquals(expectedTitle, actualResult, "Email тображается не коректно!");
+    }
+
+    @Test
+    public void getGirlName__test() {
+        //arrange
+        var expectedTitle = "test@skillbox.ru";
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.girl.click();
+        page.addEmail();
+        page.submitButton.click();
+        //assert
+        var actualResult = page.getTitleText();
+        Assertions.assertEquals(expectedTitle, actualResult, "Email тображается не коректно!");
+    }
+
+    private static Stream<Arguments> email_param() {
+        return Stream.of(
+                arguments("1231313131"),
+                arguments("ываыаываыа")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("email_param")
+    public void addInvalidEmail__firstAndSecond__test(String value) {
+        //arrange
+        var expectedErrorMessageText = "Некорректный email";
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.girl.click();
+        page.addInvalidEmail(value);
+        page.submitButton.click();
+        //assert
+        var actualResult = page.getErrorMessageText();
+        Assertions.assertEquals(expectedErrorMessageText, actualResult, "Ошибка о некоректном email не отображается!");
+    }
+
+    @Test
+    public void addInvalidEmail__third__test() {
+        //arrange
+        var expectedErrorMessageText = "Введите email";
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.girl.click();
+        page.addInvalidEmail("");
+        page.submitButton.click();
+        //assert
+        var actualResult = page.getErrorMessageText();
+        Assertions.assertEquals(expectedErrorMessageText, actualResult, "Ошибка о некоректном email не отображается!");
+    }
+
+    @Test
+    public void goToFacebookPage__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.facebookIcon.click();
+        //assert
+        var actualWindowCount = getAllWindows();
+        Assertions.assertEquals(2, actualWindowCount.size(), "Окно не открылось!");
+    }
+
+    @Test
+    public void goToVkPage__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.vkIcon.click();
+        //assert
+        var actualWindowCount = getAllWindows();
+        Assertions.assertEquals(2, actualWindowCount.size(), "Окно не открылось!");
+    }
+
+    @Test
+    public void goToTweeterPage__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.tweeterIcon.click();
+        //assert
+        var actualWindowCount = getAllWindows();
+        Assertions.assertEquals(2, actualWindowCount.size(), "Окно не открылось!");
+    }
+
+    @Test
+    public void goToInstagramPage__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.instaIcon.click();
+        //assert
+        var actualWindowCount = getAllWindows();
+        Assertions.assertEquals(2, actualWindowCount.size(), "Окно не открылось!");
+    }
+
+    @Test
+    public void footer__goToSkillboxLink__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.skillboxLink.click();
+        switchToFirstWindow();
+        driver.close();
+        switchToWindow(initialWindow);
+        //assert
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, getAllWindows().size(), "Открылось более одного окна!")
+        );
+    }
+
+    @Test
+    public void getBoyName__andHeaderPannelIsDisplayed__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.boy.click();
+        page.addEmail();
+        page.submitButton.click();
+        page.switchToParrentFrame();
+        //assert
+        var actualResult = page.header.isDisplayed();
+        Assertions.assertTrue(actualResult, "Header не отображается!");
+    }
+
+    @Test
+    public void submitEmptyInput__and__goToSkillboxLink___backToMainPage__and__errorMessageIsDisplayed__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        //act
+        page.girl.click();
+        page.submitButton.click();
+        page.switchToFrame();
+        page.skillboxLink.click();
+        switchToFirstWindow();
+        driver.close();
+        switchToWindow(initialWindow);
+        page.switchToFrame();
+        //assert
+        var actualResult = page.errorMessage.isDisplayed();
+        Assertions.assertTrue(actualResult, "Сообщение об ошибке не отображается!");
+    }
+
+    @Test
+    public void submitEmptyInput__and__goToSkillboxLink___backToMainPage__and__headerIsDisplayed__test() {
+        //arrange
+        var page = new MainPage(driver, wait);
+        page.open();
+        page.switchToFrame();
+        page.switchToFrame();
+        //act
+        page.skillboxLink.click();
+        switchToFirstWindow();
+        driver.close();
+        switchToWindow(initialWindow);
+        page.switchToFrame();
+        //assert
+        var actualResult = page.header.isDisplayed();
+        Assertions.assertTrue(actualResult, "Header не отображается!");
+    }
+}
